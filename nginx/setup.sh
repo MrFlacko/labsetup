635d89442de7bb71ad8f36ddaab9d25c55865365
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-07-09 22:21:50 +1000

    Update setup.sh

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,9 +1,10 @@
-# NGINX Setup Commands
+# NGINX Setup Commands Certbot grabbing certificate as well
 sudo apt install nginx certbot
 sudo systemctl stop nginx
 sudo ufw allow 80
 # In cloudflare, add domain while proxy is turned on 
 # Point the domain to your internal server IP (A Record)
-sudo certbot certonly -d YOUR.DOMAIN.COMip a
+sudo certbot certonly -d YOUR.DOMAIN.COM
 sudo systemctl start nginx
 # Option 1 
+sudo rm /etc/nginx/sites-available/default
