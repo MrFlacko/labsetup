405af3bdb04b9cc1a7fadc8c6433a16ac08fc193
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-07-12 01:57:42 +1000

    Update setup.sh

--- Docker/JellyFin/setup.sh
+++ Docker/JellyFin/setup.sh
@@ -7,8 +7,8 @@ sudo mkdir -p /srv/media/config/jellyseerr
 sudo chown 1000:1000 /srv/media/config/jellyseerr
 
 # SABnzbd
-sudo mkdir -p /mnt/big/SabDownloads/{Movies,Shows}
-sudo chown 1000:1000 /mnt/big/SabDownloads/{Movies,Shows}
+sudo mkdir -p /mnt/big/SabDownloads/Downloads/complete/{Movies,Shows}
+sudo chown 1000:1000 /mnt/big/SabDownloads/Downloads/complete/{Movies,Shows}
 
 # SABnzbd will default to IPV6 Address ::
 # Sonarr and Radarr will need to connect to it with 172.16.0.10 or the Server IP
