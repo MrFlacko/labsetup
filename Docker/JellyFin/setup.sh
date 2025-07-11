ff1ed30be778943b4a2ff2a59d814547622a99d4
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-07-12 01:06:26 +1000

    Update setup.sh

--- Docker/JellyFin/setup.sh
+++ Docker/JellyFin/setup.sh
@@ -5,3 +5,6 @@ sudo chown -R 1000:1000 /mnt/big/{Movies,Shows,Music,Books}
 # JellySeerr
 sudo mkdir -p /srv/media/config/jellyseerr
 sudo chown 1000:1000 /srv/media/config/jellyseerr
+sudo mkdir -p /mnt/big/SabDownloads/Movies
+sudo mkdir -p /mnt/big/SabDownloads/Shows
+sudo chown -R 1000:1000 /mnt/big/SabDownloads
