0f9694505fd0f82207125f9b3dbf7d7d31ed47f3
MrFlacko <35322515+MrFlacko@users.noreply.github.com>
2025-07-11 17:51:32 +1000

    Update setup.sh

--- ufw/setup.sh
+++ ufw/setup.sh
@@ -1,22 +1,23 @@
-# baseline
+# reset & policy
 sudo ufw --force reset
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 
-# inbound ports (IPv4+IPv6 auto-covered)
-sudo ufw allow 5055,7878,8080,8096,8989,9696/tcp comment "media apps"
-sudo ufw allow 22/tcp       # SSH
-sudo ufw allow 80,443/tcp   # HTTP/S
-sudo ufw allow 2049/tcp     # NFS
+# inbound TCP ports – IPv4-only because src is 0.0.0.0/0
+sudo ufw allow from 0.0.0.0/0 to any port 5055,7878,8080,8096,8989,9696 proto tcp comment "Used with Jellyfin"
+sudo ufw allow from 0.0.0.0/0 to any port 22   proto tcp comment "Allow SSH"
+sudo ufw allow from 0.0.0.0/0 to any port 80,443 proto tcp comment "HTTP and HTTPS"
+sudo ufw allow from 0.0.0.0/0 to any port 2049 proto tcp comment "NFS"
+sudo ufw allow from 0.0.0.0/0 to any port 19999 proto tcp comment "NetData"
 
-# inbound nets
-sudo ufw allow from 172.16.0.0/24
-sudo ufw allow from 10.0.0.0/24
-sudo ufw allow in proto igmp from 10.0.0.1 to 224.0.0.1
+# inbound trusted subnets. These are temporary
+sudo ufw allow from 172.16.0.0/24 comment "Allow connection from 172.16.0.0/24 Network in"
+sudo ufw allow from 10.0.0.0/24 comment "Allow connection from 10.0.0.0/24 Network in"
+sudo ufw allow in proto igmp from 10.0.0.1 to 224.0.0.1 comment "Allowing IGMP connections from Edge router"
 
-# outbound nets
-sudo ufw allow out to 172.16.0.0/24
-sudo ufw allow out to 10.0.0.0/24
+# outbound to those subnets. These are temporary
+sudo ufw allow out to 172.16.0.0/24 comment "Allow connection from 172.16.0.0/24 Network out"
+sudo ufw allow out to 10.0.0.0/24 comment "Allow connection from 10.0.0.0/24 Network out"
 
-sudo ufw enable
-ufw status numbered
+sudo sudo ufw enable
+sudo ufw status numbered
